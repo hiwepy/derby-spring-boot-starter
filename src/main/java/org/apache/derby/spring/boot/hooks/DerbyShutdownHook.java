2ff0b1fa1395bd42ad6a12bfd1b90e8dc9c097a3
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:24:53 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/derby/spring/boot/hooks/DerbyShutdownHook.java
+++ src/main/java/org/apache/derby/spring/boot/hooks/DerbyShutdownHook.java
@@ -30,7 +30,7 @@ import org.springframework.core.env.Environment;
  * shutdown logic will be added when the full Berkeley DB integration is wired up
  * in {@link org.apache.derby.spring.boot.DerbyAutoConfiguration}.</p>
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 public class DerbyShutdownHook extends Thread {
